ecb9ffc8f69ec60db1f7504eede02c4c560a17fc
benkips <kipbena23@gmail.com>
2020-12-12 10:08:15 +0300

    fixed viewing of files with scoped storage

--- app/src/main/java/www/digitalexperts/church_tracker/Utils/Utils.kt
+++ app/src/main/java/www/digitalexperts/church_tracker/Utils/Utils.kt
@@ -1,30 +1,30 @@
 package www.digitalexperts.church_tracker.Utils
 
+import android.content.ContentUris
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.os.ParcelFileDescriptor
 import android.provider.MediaStore
-import android.provider.MediaStore.Audio.Albums.getContentUri
-import android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-import android.provider.MediaStore.Downloads.EXTERNAL_CONTENT_URI
-import android.provider.MediaStore.Files.getContentUri
+import android.provider.OpenableColumns
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
-import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import www.digitalexperts.church_tracker.Network.Resource
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
+import www.digitalexperts.church_tracker.fragments.Radiostream
+import www.digitalexperts.church_tracker.models.Audioss
+import www.digitalexperts.church_tracker.models.Myaudioitems
+import java.io.IOException
 
 
 fun View.snackbar(message: String, action: (() -> Unit)? = null) {
@@ -47,7 +47,7 @@ fun Fragment.handleApiError(
     retry: (() -> Unit)? = null
 ) {
     when {
-        failure.isNetworkError -> requireView().snackbar("please check your internet",retry)
+        failure.isNetworkError -> requireView().snackbar("please check your internet", retry)
         else -> {
             val error = failure.errorBody?.string().toString()
             requireView().snackbar(error)
@@ -102,12 +102,172 @@ fun Context.getBitmapFromVectorDrawable(drawableId: Int): Bitmap? {
     val bitmap = Bitmap.createBitmap(
         drawable.intrinsicWidth,
         drawable.intrinsicHeight,
-        Bitmap.Config.ARGB_8888) ?: return null
+        Bitmap.Config.ARGB_8888
+    ) ?: return null
     val canvas = Canvas(bitmap)
     drawable.setBounds(0, 0, canvas.width, canvas.height)
     drawable.draw(canvas)
 
     return bitmap
+}
+
+fun Context.readmyaudios():List<Audioss>{
+
+    val audioList = mutableListOf<Audioss>()
+
+    val collection =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Audio.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL
+            )
+        } else {
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        }
+    val projection = arrayOf(
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.DISPLAY_NAME,
+        MediaStore.Audio.Media.DURATION,
+        MediaStore.Audio.Media.SIZE
+    )
+    val  directory = "Recordings"
+    val audiodir = "%" + directory + "%";
+
+    val selection = "${MediaStore.Audio.Media.RELATIVE_PATH}   like ?"
+    val selectionArgs = arrayOf(
+        audiodir
+    )
+    val sortOrder = "${MediaStore.Audio.Media.DISPLAY_NAME} ASC"
+
+    val query = this.contentResolver.query(
+        collection,
+        projection,
+        selection,
+        selectionArgs,
+        sortOrder
+    )
+    query?.use { cursor ->
+        // Cache column indices.
+        val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+        val nameColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+        val durationColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+        val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+
+        while (cursor.moveToNext()) {
+            // Get values of columns for a given video.
+            val id = cursor.getLong(idColumn)
+            val name = cursor.getString(nameColumn)
+            val duration = cursor.getInt(durationColumn)
+            val size = cursor.getInt(sizeColumn)
+
+            val contentUri: Uri = ContentUris.withAppendedId(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                id
+            )
+
+            // Stores column values and the contentUri in a local object
+            // that represents the media file.
+            audioList += Audioss(contentUri, name, duration, size)
+        }
+    }
+    return  audioList
+}
+
+fun Context.readaudios():List<Myaudioitems>{
+
+    val audioList = mutableListOf<Myaudioitems>()
+
+    val collection =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Audio.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL
+            )
+        } else {
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        }
+    val projection = arrayOf(
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.DISPLAY_NAME,
+        MediaStore.Audio.Media.DURATION,
+        MediaStore.Audio.Media.SIZE
+    )
+    val  directory = "Recordings"
+    val audiodir = "%" + directory + "%";
+
+    val selection = "${MediaStore.Audio.Media.RELATIVE_PATH}   like ?"
+    val selectionArgs = arrayOf(
+        audiodir
+    )
+    val sortOrder = "${MediaStore.Audio.Media.DISPLAY_NAME} ASC"
+
+    val query = this.contentResolver.query(
+        collection,
+        projection,
+        selection,
+        selectionArgs,
+        sortOrder
+    )
+    query?.use { cursor ->
+        // Cache column indices.
+        val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+        val nameColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+        val durationColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+        val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+
+        while (cursor.moveToNext()) {
+            // Get values of columns for a given video.
+            val id = cursor.getLong(idColumn)
+            val name = cursor.getString(nameColumn)
+            val duration = cursor.getInt(durationColumn)
+            val size = cursor.getInt(sizeColumn)
+
+            val contentUri: Uri = ContentUris.withAppendedId(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                id
+            )
+
+            // Stores column values and the contentUri in a local object
+            // that represents the media file.
+            audioList += Myaudioitems(contentUri, name, duration, size)
+        }
+    }
+    return  audioList
+}
+
+fun  Context.getNameFromContentUri(context: Context, contentUri: Uri?): String? {
+    val returnCursor: Cursor? = this.contentResolver.query(contentUri!!, null, null, null, null)
+    val nameColumnIndex: Int = returnCursor!!.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+    returnCursor.moveToFirst()
+    return returnCursor.getString(nameColumnIndex)
+}
+
+fun Context.deleteaudio(uri:Uri){
+
+   try {
+       val deleted = this.contentResolver.delete(uri, null, null)
+
+       if (deleted >= 0) {
+           Log.d("Tag", "File deleted");
+       }
+   } catch (e: Exception) {
+       Log.e(TAG, "failed to delete" + e.toString())
+     /*  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+           val recoverableSecurityException =
+               securityException as? RecoverableSecurityException
+                   ?: throw SecurityException()
+
+           val intentSender = recoverableSecurityException.userAction.actionIntent.intentSender
+
+           intentSender?.let {
+               mActivity.startIntentSenderForResult(intentSender, 0, null, 0, 0, 0, null)
+           }
+       } else {
+           throw SecurityException()
+       }*/
+   }
 }
 
 
